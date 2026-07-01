4183d154448d30238a206c003996d433b57f4103
user <user@MacBook-Pro-user.local>
2026-07-01 14:01:52 +0200

    run SCB zh-hans footer action

--- framework/components/popups/countriesDisclaimer.popup.js
+++ framework/components/popups/countriesDisclaimer.popup.js
@@ -10,43 +10,44 @@ export class CountriesDisclaimerPopup {
     this.confirmButton = page.locator(
       '[data-type="closed_countries_disclaimer_confirm"]',
     );
+
+    this.page.on("framenavigated", (frame) => {
+      console.log("NAVIGATED:", frame.url());
+    });
+
+    this.page.on("domcontentloaded", () => {
+      console.log("DOMCONTENTLOADED");
+    });
   }
 
   async closeIfVisible() {
-    const popup = this.popup;
     const btn = this.confirmButton;
 
     try {
-      await popup.waitFor({
+      await this.page.waitForLoadState("domcontentloaded");
+      await this.page.waitForTimeout(4000);
+
+      await btn.waitFor({
         state: "visible",
-        timeout: 8000,
+        timeout: 10000,
       });
 
       console.log("DISCLAIMER APPEARED");
 
+      const popup = btn.locator("..");
+
       await expect
-        .poll(
-          async () => {
-            if (this.page.isClosed()) {
-              return false;
+        .poll(async () => {
+          await popup.evaluate((popup) => {
+            const el = popup.firstElementChild;
+            if (el) {
+              console.log(el.scrollHeight);
+              console.log(el.clientHeight);
             }
-
-            await popup.evaluate((popup) => {
-              const scrollable = popup.firstElementChild;
-
-              if (scrollable) {
-                scrollable.scrollTop = scrollable.scrollHeight;
-              }
-            });
-
-            console.log("BUTTON DISABLED:", await btn.getAttribute("disabled"));
-
-            return await btn.isEnabled();
-          },
-          {
-            timeout: 15000,
-          },
-        )
+          });
+          console.log("BUTTON ENABLED:");
+          return btn.isEnabled();
+        })
         .toBe(true);
 
       await btn.click();
@@ -57,52 +58,3 @@ export class CountriesDisclaimerPopup {
     }
   }
 }
-
-// export class CountriesDisclaimerPopup {
-//   constructor(page) {
-//     this.page = page;
-
-//     page.on("close", () => {
-//       console.log("PAGE CLOSED EVENT");
-//     });
-//     page.on("framenavigated", (frame) => {
-//       console.log("NAVIGATED:", frame.url());
-//     });
-
-//     this.confirmButton = page.locator(
-//       '[data-type="closed_countries_disclaimer_confirm"]',
-//     );
-//   }
-
-//   async closeIfVisible() {
-//     const btn = this.page.locator(
-//       '[data-type="closed_countries_disclaimer_confirm"]',
-//     );
-
-//     console.log(
-//       "VISIBLE:",
-//       await btn.isVisible({ timeout: 1000 }).catch(() => false),
-//     );
-//     console.log("ENABLED:", await btn.isEnabled().catch(() => false));
-
-//     for (let i = 0; i < 3; i++) {
-//       try {
-//         await this.confirmButton.waitFor({
-//           state: "visible",
-//           timeout: 3000,
-//         });
-
-//         await expect(this.confirmButton).toBeEnabled({
-//           timeout: 10000,
-//         });
-
-//         await this.confirmButton.click();
-
-//         console.log(`DISCLAIMER CLOSED (${i + 1})`);
-//       } catch (e) {
-//         console.log(`ATTEMPT ${i + 1}:`, e.message);
-//         await this.page.waitForTimeout(1000);
-//       }
-//     }
-//   }
-// }
