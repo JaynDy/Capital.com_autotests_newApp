3edcf18447605718beb17cdf9a91a4f824bc845c
user <user@MacBook-Pro-user.local>
2026-07-06 19:55:34 +0200

    run tests:markets actions

--- framework/components/popups/countriesDisclaimer.popup.js
+++ framework/components/popups/countriesDisclaimer.popup.js
@@ -14,39 +14,46 @@ export class CountriesDisclaimerPopup {
   }
 
   async closeIfVisible() {
-    await this.confirmButton.waitFor({
-      state: "visible",
-      timeout: 10000,
-    });
+    try {
+      await this.confirmButton.waitFor({
+        state: "visible",
+        timeout: 10000,
+      });
 
-    await expect
-      .poll(
-        async () => {
-          await this.popup.evaluate((popup) => {
-            const scrollable = [...popup.querySelectorAll("*")].find(
-              (el) => el.scrollHeight > el.clientHeight,
-            );
+      await expect
+        .poll(
+          async () => {
+            await this.popup.evaluate((popup) => {
+              const scrollable = [...popup.querySelectorAll("*")].find(
+                (el) => el.scrollHeight > el.clientHeight,
+              );
 
-            if (!scrollable) {
-              throw new Error("Scrollable element not found");
-            }
+              if (!scrollable) {
+                throw new Error("Scrollable element not found");
+              }
 
-            scrollable.scrollTop = scrollable.scrollHeight;
-            scrollable.dispatchEvent(new Event("scroll", { bubbles: true }));
-          });
+              scrollable.scrollTop = scrollable.scrollHeight;
+              scrollable.dispatchEvent(new Event("scroll", { bubbles: true }));
+            });
 
-          const enabled = await this.confirmButton.isEnabled();
-          return enabled;
-        },
-        {
-          timeout: 10000,
-          intervals: [500],
-        },
-      )
-      .toBe(true);
+            const enabled = await this.confirmButton.isEnabled();
 
-    await this.confirmButton.click();
+            console.log("Button enabled:", enabled);
 
-    // console.log("Countries Disclaimer closed");
+            return enabled;
+          },
+          {
+            timeout: 10000,
+            intervals: [500],
+          },
+        )
+        .toBe(true);
+
+      await this.confirmButton.click();
+
+      // console.log("Countries Disclaimer closed");
+    } catch (e) {
+      // console.log("Countries Disclaimer skipped:", e.message);
+    }
   }
 }
